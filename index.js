4c92a5e940880600fe10700d42cd5826974b94bd
Jaco Kok <jaco.kok@fnb.co.za>
2016-03-14 12:47:14 +0200

    Added bitx api wrapper

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var http = require('http');
 var express = require('express');
 var app = express();
+var BitX = require('BitX');
+var bitx = new BitX();
 
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
@@ -9,6 +11,17 @@ http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
+
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  //res.send('Hello World!');
+
+  bitx.getTicker(function (err, ticker) {
+    if (err) {
+      res.send(err);
+    }
+    res.send(ticker);
+    console.dir(ticker);
+  });
+
 });
